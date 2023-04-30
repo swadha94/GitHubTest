d59c843e9dbd12ed22ff9dbbe2827f548d44a09c
swadha94 <103983778+swadha94@users.noreply.github.com>
2023-04-30 14:26:41 +0530

    Template usage

---
+++ Template_trial.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+template<typename T>
+
+void print(T val)
+{
+    cout<<"\n"<<val;
+}
+
+int main()
+{
+    print("Hi");
+    print(5);
+    print(5.5f);
+
+    return 0;
+}
--- Trial.cpp
+++ Trial.cpp
@@ -1,7 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int a = 100;
+int b = 10;
+void changePtr(int *x)
+{
+    x = &b;
+}
 int main()
 {
-    cout<<"Hello World!"; 
+    //int *x = &a;
+    //cout<<*x;
+    //changePtr(x);
+    //cout<<"\n"<<*x;
+
+    int i=0;
+    while(1)
+    {
+        if(i==0)
+        break;
+
+        cout<<"\nHi";
+        i++;
+    }
+
+    cout<<"\nYo";
 }
